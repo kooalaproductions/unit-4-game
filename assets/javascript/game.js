7b0a2b90a5d5571415b399693037a3a456cce318
Sky <erniesanchez619@gmial.com>
2019-07-19 19:48:06 -0700

    added text animation to game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,29 @@ var losses = 0;
 var crystalImg = ['assets/images/blue-diamond.jpeg', 'assets/images/green-diamond.jpeg', 'assets/images/pink-diamond.jpeg', 'assets/images/yellow-diamond.jpeg'];
 var count = 13;
 
+// Wrap every letter in a span
+$('.crystal-head').each(function(){
+    $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+  });
+  
+  anime.timeline({loop: true})
+    .add({
+      targets: '.crystal-head .letter',
+      scale: [4,1],
+      opacity: [0,1],
+      translateZ: 0,
+      easing: "easeOutExpo",
+      duration: 950,
+      delay: function(el, i) {
+        return 70*i;
+      }
+    }).add({
+      targets: '.ml2',
+      opacity: 0,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1000
+    });
 
 document.onkeypress = function (event) {
 
@@ -14,41 +37,31 @@ document.onkeypress = function (event) {
         count++;
         $(".key-start").remove();
         $(".crystal-head").remove();
+        $(".rule-container").remove();
         startGame();
+        console.log(count);
 
         $(".container-5").html('<div id= total-sum>' + "Your total score is: " + startNum + '</div>');
 
         $(".container-6").append('<div id= winner>' + "Wins: " + wins + '</div>');
 
         $(".container-6").append('<div id= loser>' + "Losses: " + losses + '</div>');
-    } else {
+    } else if(count === 14) {
+        count++;
+        console.log(count);
         alert("Press Enter to Begin");
+    }
+    else{
+        alert("Click on crystals!")
     }
 
 }
 
-
 function startGame() { //calls all the functions after a "enter" is pressed
-
-
-    rules();
     generateRandom();
     crystalValue();
     addNum();
-    
-
-
 }
-
-function rules() { //prints out the rules
-    $(".rule-container").append("You will be given a random number at the start of the game." + "<br>" +
-        "There are four crystals below. By clicking on a crystal you will add a specific amount of points to your total score." + "<br>" +
-        "You win the game by matching your total score to random numbe, you lose the game if your total score goes above the random number" + "<br>" +
-        "The value of each crystal is hidden from you until you click on it. " + "<br>" +
-        "Each time when the game starts, the game will change the values" + "<br>");
-
-}
-
 
 function generateRandom() { //creates random number the user has to match
     randomNum = Math.floor(Math.random() * 102) + 19;
@@ -63,7 +76,7 @@ function crystalValue() {
         crystal.addClass("crystal-" + i, "col-lg-12");
         crystal.addClass("col-lg-12");
         crystal.attr("src", crystalImg[i]);
-        crystal.attr("height", "50");
+        crystal.attr("height", "100");
         crystal.attr("value", (Math.floor(Math.random() * 12) + 1));
         $(".cards").append(crystal);
     }
@@ -83,23 +96,18 @@ function addNum() { //adds the value when crystals are clicked
         if (startNum === randomNum) {
             winnerNum();
         }
-
-
-
         $(".container-5").html('<div id= total-sum>' + "Your total score is: " + startNum + '</div>');
 
         //   $("#total-sum").html("this is the sum:" +startNum);
         console.log("this is the total number for user: " + startNum)
 
     });
-
-
 }
 
 function winnerNum() {
     wins++;
     $("#winner").html("Wins: "+ wins);
-    // $(".container-6").html('<div id= winner>' + "Wins: " + wins + '</div>');
+    $(".container-5-6").html('<div id= result>' + "You Won!!!" +'</div>');
 
     resetGame();
 }
@@ -107,12 +115,11 @@ function winnerNum() {
 function loserNum() {
     losses++;
     $("#loser").html("Losses: "+ losses);
-    // $(".container-6").html('<div id= loser>' + "Losses: " + losses + '</div>');
+    $(".container-5-6").html('<div id= result>' + "You Lost!!!" +'</div>');
 
     resetGame();
 
 }
-
 
 function resetGame() { //resets the game
     randomNum = Math.floor(Math.random() * 102) + 19;
@@ -123,6 +130,4 @@ function resetGame() { //resets the game
     }
   startNum = 0;
     $(".container-5").html('<div id= total-sum>' + "Your total score is: " + startNum + '</div>');
-
-
 }
